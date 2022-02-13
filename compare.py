8b59ae1d40cffc8ed01d7faec952df12c3e07637
sattisusmitha <99264212+sattisusmitha@users.noreply.github.com>
2022-02-13 07:16:24 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,15 @@
+#program to compare two numbers
+num1=int(input('enter the first number:'))
+num2=int(input('enter second number:'))
+if num1>num2:
+  print('num1 is greater than num2')
+elif num1<num2:
+ print('num1 is less than num2')
+else:
+    print('num1 is equal to num2')
+  
+
+output:
+enter first number:37
+enter second number:26
+num1 is greater than num2
